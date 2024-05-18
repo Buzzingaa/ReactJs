190e333dbe2bcb432da01059223abc448e276f0e
Buzzingaa <arjunbhusal684@gmail.com>
2024-05-18 17:55:47 +0545

    passing function as props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 
 
 import './App.css';
+import React, { useState } from 'react'
+
 import Navbar from './components/Navbar';
-import AboutUs from './components/AboutUs';
+// import AboutUs from './components/AboutUs';
+import Textform from './components/Textform';
+
 function App() {
+  const[mode,setMode]=useState('light')
+  const toggleSwitch=()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.background='black'
+
+    }
+    else{
+      setMode('light')
+      document.body.style.background='white'
+    }}
   return (
 <>
-  < Navbar title="TextSearch" aboutText="About TextUtils" />
-{/* <div className='container my-3 '>  <Textform title="enter the text here"/></div> */}
-<div style={{ width: '75%' }} className='container-sm my-5'> <AboutUs/></div>
+  < Navbar title="TextSearch" aboutText="About TextUtils"  mode={mode} fun={toggleSwitch}/>
+<div className='container my-3 '>  <Textform title="enter the text here" mode={mode}/></div>
+{/* <div style={{ width: '75%' }} className='container-sm my-5'> <AboutUs/></div> */}
 
 </>
   );
 }
+
 
 export default App;
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 export default function Textform(props) {
     
-    const[text,setText]=useState("enter your text here");
+    const[text,setText]=useState("");
     let upperText= text.toUpperCase();
     const handelOnCLick = ()=> {
         console.log("you clicked the button");
@@ -36,22 +36,22 @@ export default function Textform(props) {
   return (
     <>
 
-<div>
+<div style={{color:props.mode==='light'?'black':'white'}}>
     <h2>{props.title}</h2>
     <div className="mb-3" >
-    <textarea className="form-control " id="myBox" rows="4"  value={text} onChange={handleOnChange}></textarea>
+    <textarea className="form-control " style={{backgroundColor:props.mode==='light'?'white':'gray'}} id="myBox" rows="4"  value={text} onChange={handleOnChange}></textarea>
     <button className="btn btn-primary my-3" onClick={handelOnCLick}>convertToUppercase</button>
     <button className="btn btn-secondary my-3 mx-3" onClick={(handleLowClick)}>convertToLowercase</button>
     <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2" id="toggle">Speak</button>
 
     </div>
     </div>
-    <div className="container">
+    <div className="container" style={{color:props.mode==='light'?'black':'white'}}>
       <h1>Your text summary</h1>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p> {0.008*text.split(" ").length } minute read</p>
       <h2>Priview</h2>
-      <p>{text}</p>
+      <p>{text==''?'write the text above to see priview':text}</p>
 
     </div>
     
