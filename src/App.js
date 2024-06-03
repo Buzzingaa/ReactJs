73816069060cd153bb308f7f85f6d714f6aba752
Buzzingaa <arjunbhusal684@gmail.com>
2024-06-03 16:55:40 +0545

    react router

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import './App.css';
 import React, { useState } from 'react'
 
 import Navbar from './components/Navbar';
-// import AboutUs from './components/AboutUs';
+import AboutUs from './components/AboutUs';
 import Textform from './components/Textform';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+  
+} from "react-router-dom";
+
 
 function App() {
   const[alert,setAlert]=useState(null)
@@ -35,13 +42,22 @@ function App() {
       showAlert('light mode enabled','success')
     }}
   return (
-<>
-  < Navbar title="TextSearch" aboutText="About TextUtils"  mode={mode} fun={toggleSwitch}/>
+<Router>
+< Navbar title="TextSearch" aboutText="About TextUtils"  mode={mode} fun={toggleSwitch}/>
   <Alert alert={alert}/>
-<div className='container my-3 '>  <Textform title="enter the text here" mode={mode} showAlert={showAlert}/></div>
-{/* <div style={{ width: '75%' }} className='container-sm my-5'> <AboutUs/></div> */}
+<Routes>
+         <Route  exact path="/about" element={<div style={{ width: '75%' }} className='container-sm my-5'> <AboutUs/></div>} />
+</Routes>
+<Routes>
 
-</>
+          <Route  exact path="/" element={<div className='container my-3 '>  <Textform title="enter the text here" mode={mode} showAlert={showAlert}/></div>} />
+
+   </Routes>
+  
+
+
+
+</Router>
   );
 }
 
